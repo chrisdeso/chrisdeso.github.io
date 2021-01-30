f1e94959ceaa22b8fa9b0b85b995f804fa862d37
chrisdeso <cdesousa89@gmail.com>
2021-01-29 22:07:07 -0600

    Add ToDo component, component props, and bind props to input attribute in todo component

--- hello-world/README.md
+++ hello-world/README.md
@@ -22,3 +22,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Notes
+
+```
+
+A component should never alter the value of its own props. This makes debugging hard. https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_first_component#:~:text=a%20component%20should,its%20own%20props
+```
--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>chrisdeso</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item" v-bind:done="true"></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ToDoItem from './components/ToDoItem.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld,
+    ToDoItem,
   },
 };
 </script>
