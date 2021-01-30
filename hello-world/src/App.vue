e1023cf8f623eab280a06332e7967bd4c87fea38
chrisdeso <cdesousa89@gmail.com>
2021-01-29 22:53:46 -0600

    Make unique id into prop, remove lodash import from todoitem component

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,20 +2,33 @@
   <div id="app">
     <h1>chrisdeso</h1>
     <ul>
-      <li>
-        <to-do-item label="My ToDo Item" v-bind:done="true"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import uniqueId from 'lodash.uniqueid';
 import ToDoItem from './components/ToDoItem.vue';
 
 export default {
   name: 'app',
   components: {
     ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Build personal website', done: false },
+        { id: uniqueId('todo-'), label: 'Walk the dogs', done: true },
+        { id: uniqueId('todo-'), label: 'Eat a twinky', done: true },
+        { id: uniqueId('todo-'), label: 'Schedule time off', done: false },
+        { id: uniqueId('todo-'), label: 'Update resume', done: true },
+      ],
+    };
   },
 };
 </script>
